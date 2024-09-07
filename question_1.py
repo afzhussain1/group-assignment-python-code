1e2108eed16a722151b244022c4eaa6f15c81f6e
afzhussain1 <muhammad.afzaal@cqumail.com>
2024-09-07 17:49:04 +0600

    testing the code path

---
+++ question_1.py
@@ -0,0 +1,9 @@
+import zipfile
+import os
+import pandas as pd
+
+# Define paths
+zip_path = r'C:\Users\Speed\Desktop\group-assignment\csv_.zip'  # Path to the zipped folder
+extract_path = r'C:\Users\Speed\Desktop\group-assignment\csv_extracted'  # Path where you want to extract the CSV files
+output_txt_file = r'C:\Users\Speed\Desktop\group-assignment\textfile.txt'  # Path where you want to save the combined text
+
