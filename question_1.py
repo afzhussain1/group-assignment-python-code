290295d351906d189bb70a1117d4e3b8238c3a20
afzhussain1 <muhammad.afzaal@cqumail.com>
2024-09-10 19:43:48 +0600

    question1 and txt file is made which read the  given CSV files

--- question_1.py
+++ question_1.py
@@ -1,9 +1,34 @@
+
 import zipfile
-import os
 import pandas as pd
+import os
 
-# Define paths
-zip_path = r'C:\Users\Speed\Desktop\group-assignment\csv_.zip'  # Path to the zipped folder
-extract_path = r'C:\Users\Speed\Desktop\group-assignment\csv_extracted'  # Path where you want to extract the CSV files
-output_txt_file = r'C:\Users\Speed\Desktop\group-assignment\textfile.txt'  # Path where you want to save the combined text
+#paths and please change the paths according to your location thanks
+zip_path = r'D:\assgn\group-assignment-python-code\csv_.zip'  # Path to the ZIP file
+extracted_path = r'D:\assgn\group-assignment-python-code\csv_extracted'  # please make a extracted folder
+output_txt_path = r'D:\assgn\group-assignment-python-code\outputtxt.txt'  # Path of text file
 
+# Extraction the zip files and the save them into extracted folder
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall(extracted_path)
+
+# empty array to collect the files
+all_texts = []
+#for loops to read the file
+for root, dirs, files in os.walk(extracted_path):
+    for file in files:
+        if file.endswith('.csv'):  # if the file is a CSV
+            file_path = os.path.join(root, file)
+            df = pd.read_csv(file_path)
+            
+            # if condtion to check the colomn name
+            if 'TEXT' in df.columns:
+                texts = df['TEXT'].dropna().tolist() 
+                all_texts.extend(texts)  
+
+# show txt in files
+with open(output_txt_path, 'w', encoding='utf-8') as f:
+    for text in all_texts:
+        f.write(text + '\n')  
+
+print(f'Whole texts have been saved to {output_txt_path}')
