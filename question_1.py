d568c4822a1c6e03a06bda1095f68877790241a5
afzhussain1 <muhammad.afzaal@cqumail.com>
2024-09-10 20:21:50 +0600

    Taken the column name from user

--- question_1.py
+++ question_1.py
@@ -1,50 +1,9 @@
 
-# import zipfile
-# import pandas as pd
-# import os
-
-# #paths and please change the paths according to your location thanks
-# zip_path = r'D:\assgn\group-assignment-python-code\csv_.zip'  # Path to the ZIP file
-# extracted_path = r'D:\assgn\group-assignment-python-code\csv_extracted'  # please make a extracted folder
-# output_txt_path = r'D:\assgn\group-assignment-python-code\outputtxt.txt'  # Path of text file
-
-# # Extraction the zip files and the save them into extracted folder
-# with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-#     zip_ref.extractall(extracted_path)
-
-# # empty array to collect the files
-# all_texts = []
-# #for loops to read the file
-# for root, dirs, files in os.walk(extracted_path):
-#     for file in files:
-#         if file.endswith('.csv'):  # if the file is a CSV
-#             file_path = os.path.join(root, file)
-#             df = pd.read_csv(file_path)
-            
-#             # if condtion to check the colomn name
-#             if 'TEXT' in df.columns:
-#                 texts = df['TEXT'].dropna().tolist() 
-#                 all_texts.extend(texts)  
-
-# # show txt in files
-# with open(output_txt_path, 'w', encoding='utf-8') as f:
-#     for text in all_texts:
-#         f.write(text + '\n')  
-
-# print(f'Whole texts have been saved to {output_txt_path}')
-
-
-
-
-
-
-
-
-
 import zipfile
 import pandas as pd
 import os
-
+#take the column from the user
+column_name = input("Please enter the column name to extract text from: ")
 #paths and please change the paths according to your location thanks
 zip_path = r'D:\assgn\group-assignment-python-code\csv_.zip'  # Path to the ZIP file
 extracted_path = r'D:\assgn\group-assignment-python-code\csv_extracted'  # Directory for extracted files
@@ -68,11 +27,11 @@ for root, dirs, files in os.walk(extracted_path):
                 df = pd.read_csv(file_path)
                 
                 # Check if 'TEXT' column exists in the CSV file
-                if 'TEXT' in df.columns:
-                    texts = df['TEXT'].dropna().tolist()  
+                if column_name  in df.columns:
+                    texts = df[column_name].dropna().tolist()  
                     all_texts.extend(texts)
                 else:
-                    print(f"Warning: 'TEXT' column is not found in path {file_path}")
+                    print(f"Warning: {column_name} column is not found in path {file_path}")
 
             except pd.errors.EmptyDataError:
                 print(f"Warning: {file_path} is empty file.")
