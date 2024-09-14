0e906b1caed2a06ca341336b840d91106df8fb01
nabil83321 <nabilkhan5122@gmail.com>
2024-09-14 21:22:13 +0600

    added validations on the code and handle exaction and errors

--- question_2_ch_2.1.py
+++ question_2_ch_2.1.py
@@ -1,15 +1,20 @@
 import string
 
 def decryptText(ciphertext, shift):
-    # initalizing the empty array
+    if not isinstance(ciphertext, str):
+        raise ValueError("Text must be a string.")
+    if not isinstance(shift, int) or shift < 0 or shift > 25:
+        raise ValueError("Shift must be an integer between 0 and 25.")
+    
+    # Initializing the empty array
     tempText = []
     for char in ciphertext:
         if char in string.ascii_uppercase:
-            # convert it into uppercase letters
+            # Convert it into uppercase letters
             decryptedCharactor = chr((ord(char) - shift - ord('A')) % 26 + ord('A'))
             tempText.append(decryptedCharactor)
         elif char in string.ascii_lowercase:
-            # convert it into lowercase letters
+            # Convert it into lowercase letters
             decryptedCharactor = chr((ord(char) - shift - ord('a')) % 26 + ord('a'))
             tempText.append(decryptedCharactor)
         else:
@@ -17,18 +22,27 @@ def decryptText(ciphertext, shift):
     return ''.join(tempText)
 
 def findDecryptionCode(ciphertext):
-    for shift in range(1, 26):  # Test charactors 1 to 25
-        tempText = decryptText(ciphertext, shift)
-        print(f"Shift {shift}: {tempText}")
-        
+    if not isinstance(ciphertext, str):
+        raise ValueError("Cipher text must be a string.")
+    
+    for shift in range(1, 26):  # Test shifts 1 to 25
+        try:
+            tempText = decryptText(ciphertext, shift)
+            print(f"Shift {shift}: {tempText}")
+        except ValueError as e:
+            print(f"Error {shift}: {e}")
+
 def main():
     # Example ciphertext
     ciphertext = ("VZ SDSKLWER LKJGD HE LEWWR EWTECSDF KSEJ V NZ BHG BS PBJSDFSDG "
                   "HSKDJHB MBJWUIR BCJSGF JJAGUWRG JRUI LKUOIQWUROIW JHKJHKJO "
                   "ZMNCBXMV LKJLWE")
     
-    print("Decrypting the cryptogram...")
-    findDecryptionCode(ciphertext)
+    try:
+        print("Decrypting the cryptogram...")
+        findDecryptionCode(ciphertext)
+    except ValueError as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
     main()
